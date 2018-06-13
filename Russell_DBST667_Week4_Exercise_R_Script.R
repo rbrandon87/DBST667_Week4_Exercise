cb11545d75bad4d9ee7c5ffbc0848b5e4dfb0d93
rbrandon87 <rbrandon87@gmail.com>
2018-06-12 21:31:45 -0400

    Done
    Done

--- Russell_DBST667_Week4_Exercise_R_Script.R
+++ Russell_DBST667_Week4_Exercise_R_Script.R
@@ -11,47 +11,49 @@ newcredit<-read.csv(file="newcredit.csv", head=TRUE, sep=",")
 #Observe credit structure
 str(credit) 
 str(newcredit)
-#Data pre-processing
+
+#Part 2bi Data pre-processing
+#Replace missing values with mean value
+apply(credit, 2, function (credit) sum(is.na(credit)))
+credit$A2[is.na(credit$A2)]<-mean(credit$A2, na.rm=TRUE)
+credit$A14[is.na(credit$A14)]<-mean(credit$A14, na.rm=TRUE)
+apply(credit, 2, function (credit) sum(is.na(credit)))
+
+#Part 2cii Split data between training and test sets
+set.seed(1234)
+ind <- sample(2, nrow(credit), replace = TRUE, prob = c(0.7, 0.3))
+train.data <- credit[ind == 1, ]
+test.data <- credit[ind == 2, ]
+
+#Part 2di Run method on credit training data
+myFormula<-class~.
+credit_ctree <- ctree(myFormula, data = train.data)
+
+#Part 2dii Print credit tree structure
+print(credit_ctree) 
+
+#Part 2de Visualize credit tree
+plot(credit_ctree, main = "Credit Approval", type = "simple")
+
+#Part 2fi Generate a confusion matrix
+table(predict(credit_ctree), train.data$class)
+prop.table(table(predict(credit_ctree), train.data$class))
+
+#Part 2gi Evaluate the credit model on a test data
+testPred <- predict(credit_ctree, newdata = test.data)
+table (testPred, test.data$class)
+prop.table(table(testPred, test.data$class))
+
+#Data Preprocess newcredit data frame before use
 #Fix "A1" attribute name in newcredit
 colnames(newcredit)[1] <- "A1"
 #Factor class attribute in newcredit
 newcredit$class<-factor(newcredit$class)
 #Convert int into num type to match credit
 newcredit$A14<-as.numeric(newcredit$A14)
-#Replace missing values with mean value
 apply(newcredit, 2, function (newcredit) sum(is.na(newcredit)))
-apply(credit, 2, function (credit) sum(is.na(credit)))
-credit$A2[is.na(credit$A2)]<-mean(credit$A2, na.rm=TRUE)
-credit$A14[is.na(credit$A14)]<-mean(credit$A14, na.rm=TRUE)
-apply(credit, 2, function (credit) sum(is.na(credit)))
 
-#Split data between training and test sets
-set.seed(1234)
-ind <- sample(2, nrow(credit), replace = TRUE, prob = c(0.7, 0.3))
-train.data <- credit[ind == 1, ]
-test.data <- credit[ind == 2, ]
-
-#Run method on credit training data
-myFormula<-class~.
-credit_ctree <- ctree(myFormula, data = train.data)
-
-#Print credit tree structure
-print(credit_ctree) 
-
-#Visualize credit tree
-nodes(credit_ctree, 2)
-plot(credit_ctree)
-plot(credit_ctree, type="simple")
-
-# Generate a confusion matrix
-table(predict(credit_ctree), train.data$class)
-prop.table(table(predict(credit_ctree), train.data$class))
-
-#Evaluate the credit model on a test data
-testPred <- predict(credit_ctree, newdata = test.data)
-table (testPred, test.data$class)
-
-#Predict class values in newcredit
+#Part 2hi Predict class values in newcredit
 newcreditPred <- predict(credit_ctree, newdata = newcredit)
 table (newcreditPred, newcredit$class)
 newcreditPred
